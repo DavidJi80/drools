0713fb8fce1876110a52f0baf654e446131da072
jizhou <297933227@qq.com>
2021-02-17 23:19:15 +0800

    in home

--- src/main/java/com/benetech/demo/drools/configs/DroolsAutoConfiguration.java
+++ src/main/java/com/benetech/demo/drools/configs/DroolsAutoConfiguration.java
@@ -5,6 +5,7 @@ import org.kie.api.KieServices;
 import org.kie.api.builder.KieBuilder;
 import org.kie.api.builder.KieFileSystem;
 import org.kie.api.builder.KieRepository;
+import org.kie.api.builder.ReleaseId;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 import org.kie.internal.io.ResourceFactory;
